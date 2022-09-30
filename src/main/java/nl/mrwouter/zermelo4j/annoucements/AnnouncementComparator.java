61280202aec876d7ad7ddf44fe2e7204263d9018
MrWouterNL <wouter@dedroog.com>
2022-10-01 00:39:58 +0200

    Switch to 4 spaces instead of tabs

--- src/main/java/nl/mrwouter/zermelo4j/annoucements/AnnouncementComparator.java
+++ src/main/java/nl/mrwouter/zermelo4j/annoucements/AnnouncementComparator.java
@@ -4,18 +4,11 @@ import java.util.Comparator;
 
 /**
  * Comparator used to make sure that all announcements are sorted from start to beginning.
- *
  */
 public class AnnouncementComparator implements Comparator<Announcement> {
 
-	@Override
-	public int compare(Announcement ann1, Announcement ann2) {
-        if (ann1.getStart() == ann2.getStart()) {
-            return 0;
-        }
-        if (ann1.getStart() < ann2.getStart()) {
-            return -1;
-        }
-        return 1;
-	}	
+    @Override
+    public int compare(Announcement ann1, Announcement ann2) {
+        return Long.compare(ann1.getStart(), ann2.getStart());
+    }
 }
