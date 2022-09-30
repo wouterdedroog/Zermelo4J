e69c5685cdea37c2637cba0f011fa52c7c306ab3
MrWouterNL <wouter@dedroog.com>
2022-09-30 21:20:36 +0200

    Update gson, move source to src/main/java directory

--- src/nl/mrwouter/zermelo4j/annoucements/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Package that contains the announcement object for Zermelo4J
- */
-package nl.mrwouter.zermelo4j.annoucements;
--- src/nl/mrwouter/zermelo4j/appointments/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Package that contains the appointment objects for Zermelo4J
- */
-package nl.mrwouter.zermelo4j.appointments;
