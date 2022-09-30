8f87165efc16155e96b5308a3ef2ed06f466560d
MrWouterNL <wouter@dedroog.com>
2022-09-30 21:20:36 +0200

    Update gson, move source to src/main/java directory

---
+++ src/main/java/nl/mrwouter/zermelo4j/appointments/AppointmentComparator.java
@@ -0,0 +1,21 @@
+package nl.mrwouter.zermelo4j.appointments;
+
+import java.util.Comparator;
+
+/**
+ * Comparator used to make sure that all appointments are sorted from start to beginning.
+ *
+ */
+public class AppointmentComparator implements Comparator<Appointment> {
+
+	@Override
+	public int compare(Appointment app1, Appointment app2) {
+        if (app1.getStart() == app2.getStart()) {
+            return 0;
+        }
+        if (app1.getStart() < app2.getStart()) {
+            return -1;
+        }
+        return 1;
+	}	
+}
---
+++ src/main/java/nl/mrwouter/zermelo4j/appointments/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package that contains the appointment objects for Zermelo4J
+ */
+package nl.mrwouter.zermelo4j.appointments;
